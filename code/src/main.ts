a6380378d76750ddbf8383e88c7d0d4c971b58c2
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-05-13 16:36:07 +0200

    feat!: initial mod

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,6 +1,4 @@
-
-import { createRepository } from 'repository-script'
-import { createPatch } from 'quickentity-script'
+import { createPatch, CommonPaths } from 'quickentity-script'
 import { mkdir } from 'fs/promises'
 import path from 'path'
 
@@ -11,32 +9,19 @@ async function savePatch(patch: any, name: string, chunk: number = 0) {
   await patch.save(path.join(chunkFolder, `${name}.entity.patch.json`))
 }
 
-async function saveRepo(repo: any, name: string = 'repo', chunk: number = 0) {
-  const chunkFolder = path.join('..', 'content', `chunk${chunk}`)
-
-  await mkdir(chunkFolder, { recursive: true })
-  await repo.save(path.join(chunkFolder, `${name}.repository.json`))
-}
-
 async function createMainPatch() {
-  const patch = createPatch('', '')
+  const patch = createPatch(CommonPaths.Safehouse)
+  const door = patch.getEntity('ca130857153cc016')
+  const root = door.addChild({ name: 'Kitchen Door Open [MOD]', ...CommonPaths.Entity })
 
-  await savePatch(patch, 'main')
-}
+  root.addOnGameStartListener({ OpenDoor: door })
 
-async function createRepositoryPatch() {
-  const repo = createRepository()
-
-  await saveRepo(repo)
+  await savePatch(patch, 'safehouse', 29)
 }
 
 async function main() {
   console.log('Making main patch...')
   await createMainPatch()
-  console.log('Done.')
-
-  console.log('Making main repository patch...')
-  await createRepositoryPatch()
   console.log('Done.')
 }
 
